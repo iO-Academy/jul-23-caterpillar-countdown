d685930f138b656e527f4e8893651e9b4c491b82
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-15 14:19:48 +0100

    Peer changes

---
+++ src/timer.js
@@ -0,0 +1,13 @@
+let timer = function(i) {
+    document.querySelector('#timer').textContent = i + ' seconds remaining'
+    let timeLeft = setTimeout(() => {timer(--i)}, 1000)
+    if (i === 0) {
+        clearTimeout(timeLeft)
+    }
+}
+document.querySelector('#button').addEventListener('click', () =>{
+    timer(30)
+}) 
+
+
+
