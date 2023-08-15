45c2618012553e96e8559700a9b82d998466dcca
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-15 14:16:12 +0100

    Story3Task1and2Complete

--- src/Story3SingleRandomNumber.js
+++ src/Story3SingleRandomNumber.js
@@ -1,25 +1,13 @@
-// let randomNumber = Math.floor(Math.random() * 101)
-// document.querySelector('.singleRandomNumber').textContent = randomNumber
-const numberArray = [];
-for (i = 0; i <= 100; i++) {
-    numberArray.push(i)  
+
+
+function randomInt(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-const firstRandomNumber = Math.floor(Math.random() * 101)
-const secondRandomNumber = Math.floor(Math.random() * 101)
-const thirdRandomNumber = Math.floor(Math.random() * 101)
-const fourthdRandomNumber = Math.floor(Math.random() * 101)
-const fifthRandomNumber = Math.floor(Math.random() * 101)
-document.querySelector('.firstRandomNumber').textContent = firstRandomNumber
-document.querySelector('.secondRandomNumber').textContent = secondRandomNumber
-document.querySelector('.thirdRandomNumber').textContent = thirdRandomNumber
-document.querySelector('.fourthRandomNumber').textContent = fourthdRandomNumber
-document.querySelector('.fifthRandomNumber').textContent = fifthRandomNumber
+function createMultipleCardsBetween3And5(){
+    for (let i = 0; i < randomInt(2,6); i++) {
+        document.querySelector('.cardGenerator').textContent += randomInt(0,101) + ' '
+    }
+} 
+createMultipleCardsBetween3And5()
 
-console.log(threeToFiveRandomNumbers)
-
-function getMulpileRamdom(numberArray) {
-    const shuffled = [numberArray].sort(() => 0.5 - Math.random())
-    return shuffled.slice()
-}
-console.log(Math.random())
