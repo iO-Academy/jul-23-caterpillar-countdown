a598f74fd70e4ef55f48ea3dadea5dac1f98b42e
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-15 16:48:24 +0100

    Story3Task1,2 and 3 changes made in line with peer review ready for trainer review

--- src/generateNumberCards.js
+++ src/generateNumberCards.js
@@ -1,25 +1,25 @@
-function randomInt(max, min) {
+function generateRandomInt(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function createMultipleCardsBetween3And5(){
+function generate3To5NumberCards(){
 let randomNumbersHolder = {}
-    for (let i = 0; i < randomInt(5,3); i++) {
-        let randomNumber = randomInt(100, 0)
+    for (let i = 0; i < generateRandomInt(5,3); i++) {
+        let randomNumber = generateRandomInt(100, 0)
         while (Object.values(randomNumbersHolder).includes(randomNumber)) {
-            randomNumber = randomInt(100, 0)
+            randomNumber = generateRandomInt(100, 0)
         } 
         randomNumbersHolder[i] = randomNumber
     }
 
-    Object.values(randomNumbersHolder).forEach(number => {
-        let content = document.querySelector('.number-cards-container').innerHTML
-        content +=  
-            `<div class="number-card">
-                <p>${number}</p>
-            </div>`
-        document.querySelector('.number-cards-container').innerHTML = content
-        console.log(number)
-    })
-} 
-createMultipleCardsBetween3And5()
+Object.values(randomNumbersHolder).forEach(number => {
+    let content = document.querySelector('.number-cards-container').innerHTML
+    content +=  
+        `<div class="number-card">
+            <p>${number}</p>
+        </div>`
+    document.querySelector('.number-cards-container').innerHTML = content
+})
+}
+
+generate3To5NumberCards()
