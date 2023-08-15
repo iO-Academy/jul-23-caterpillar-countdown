61a01c00bd0850998bce31ac29778b21e62ca419
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-15 15:40:28 +0100

    3 - 5 unique numbers generated and stored in an object called randomNumbersHolder

--- src/Story3SingleRandomNumber.js
+++ src/Story3SingleRandomNumber.js
@@ -1,5 +1,3 @@
-
-
 function randomInt(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
@@ -7,10 +5,15 @@ function randomInt(max, min) {
 function createMultipleCardsBetween3And5(){
 let randomNumbersHolder = {}
     for (let i = 0; i < randomInt(5,3); i++) {
-        randomNumbersHolder[i] = randomInt(100, 0)
-        // document.querySelector('.cardGenerator').textContent += randomInt(100, 0) + ' '
+        let randomNumber = randomInt(100, 0)
+        while (Object.values(randomNumbersHolder).includes(randomNumber)) {
+            randomNumber = randomInt(100, 0)
+        } 
+        randomNumbersHolder[i] = randomNumber
     }
     console.log(randomNumbersHolder)
+    // console.log(Object.values(randomNumbersHolder).includes(50))
     // document.querySelector('.cardGenerator').textContent = randomNumbersHolder
 } 
+
 createMultipleCardsBetween3And5()
