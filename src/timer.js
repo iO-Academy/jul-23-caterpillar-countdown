7e8a451d94dcdd65262b4ca644249d1465f66536
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-15 16:46:16 +0100

    Ready for second trainer review

--- src/timer.js
+++ src/timer.js
@@ -1,13 +1,13 @@
 const timer = timeLeft => {
     document.querySelector('.timer').textContent = 'Time Left: ' + timeLeft + ' seconds'
     const countdown = setTimeout(() => {timer(--timeLeft)}, 1000)
+    timer(30)
     if (timeLeft === 0) {
         clearTimeout(countdown)
         alert('timer done')
     }
 }
 
-timer(30)
 
 
 
