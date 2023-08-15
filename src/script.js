ffd452dcdd216753b370cf94df3c9533bf02326e
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-15 11:17:15 +0100

    Timer displayed and stops at 0 with an alert

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
-let gameTimer = setTimeout(() => {
-    console.log('timer done')
-}, 30000)
+/* function startTimer()
+{
+    document.querySelector('submit').addEventListener('click', () => {
+        setTimeout(() => {
+            console.log('timer done')
+        }, 30000)
+    })
+} */
+
+
+let timer = function(x) {
+    document.querySelector('#timer').innerHTML = x + ' seconds remaining'
+    let timeLeft = setTimeout(() => {timer(--x)}, 1000)
+    if (x === 0) {
+        clearTimeout(timeLeft)
+        alert('timer done')
+    }
+}
+timer(30)
