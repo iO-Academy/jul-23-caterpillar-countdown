94c912d4067c06a784a57ae7849423f1494248ba
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-16 16:55:45 +0100

    numbers unhighlight if pressed incorrectly. still a lot of bugs though

--- src/highlight.js
+++ src/highlight.js
@@ -1,14 +1,22 @@
 const cards = document.querySelectorAll(".number-card")
 
 let randomNumbersHolderSequential = randomNumbersHolder.sort((function(a, b){return a-b}))
-
+let correctNumbersHolder = []
 cards.forEach(function (card) {
   card.addEventListener("click", (e) => {
+    
     if (e.target.textContent == Math.min(...randomNumbersHolderSequential)) {
       e.target.style.backgroundColor = "#686868"
-      randomNumbersHolderSequential.shift(randomNumbersHolderSequential)
+      correctNumbersHolder.push(randomNumbersHolderSequential.shift(randomNumbersHolderSequential))
       console.log(e.target.textContent) //leaving this in for assistance with PR
       console.log(randomNumbersHolderSequential) //leaving this in for assistance with PR
+      console.log(correctNumbersHolder)
+    } else {
+      randomNumbersHolderSequential = correctNumbersHolder.concat(randomNumbersHolderSequential)
+      console.log(randomNumbersHolderSequential)
+      cards.forEach(card => {
+        card.style.backgroundColor = "#cd853f"
+      })
     }
   })
 })
