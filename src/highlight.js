be16d047058b240edd16a33b1e5e059dc67bb046
Cosmin <cosmin_7d@yahoo.com>
2023-08-16 10:08:57 +0100

    bits added

--- src/highlight.js
+++ src/highlight.js
@@ -1,5 +1,7 @@
 const cards = document.querySelectorAll(".number-card");
 
+const numbers = [4, 76, 23, 98];
+
 cards.forEach(function (card) {
   card.addEventListener("click", (e) => {
     e.target.style.backgroundColor = "#686868";
