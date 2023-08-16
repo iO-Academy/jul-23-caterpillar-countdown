52ec81c891c1dc656471e1085602633308943154
Cosmin <cosmin_7d@yahoo.com>
2023-08-16 10:21:55 +0100

    deleted array

--- src/highlight.js
+++ src/highlight.js
@@ -1,7 +1,5 @@
 const cards = document.querySelectorAll(".number-card");
 
-const numbers = [4, 76, 23, 98];
-
 cards.forEach(function (card) {
   card.addEventListener("click", (e) => {
     e.target.style.backgroundColor = "#686868";
