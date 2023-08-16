340135bd9de46010c1e4d71987bc5472c788495d
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-16 13:08:32 +0100

    numbers highlight if pressed in order

--- src/highlight.js
+++ src/highlight.js
@@ -1,8 +1,24 @@
 const cards = document.querySelectorAll(".number-card");
-console.log(cards);
+
+let randomNumbersHolderSequential = randomNumbersHolder.sort((function(a, b){return a-b}))
+
 cards.forEach(function (card) {
   card.addEventListener("click", (e) => {
+    if (e.target.textContent == Math.min(...randomNumbersHolderSequential))
     e.target.style.backgroundColor = "#686868";
-    console.log(e.target);
+    randomNumbersHolderSequential.shift(randomNumbersHolderSequential)
+    console.log(e.target.textContent);
   });
 });
+
+// randomNumbersHolderSequential.forEach(numbers => {
+//   cards.forEach(function (card) {
+//     card.addEventListener("click", (e) => {
+//       if (e.target.textContent == number){
+//         e.target.style.backgroundColor = "#686868"
+//       }
+      
+//     });
+// })
+
+
