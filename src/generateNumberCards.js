53875929c5e0977b0312d0b6d509d7f38aac1402
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-16 09:51:06 +0100

    peer reviewed indentation issues, ready for TR Pass 2

--- src/generateNumberCards.js
+++ src/generateNumberCards.js
@@ -3,7 +3,7 @@ function generateRandomInt(min, max) {
 }
 
 function generate3To5NumberCards() {
-let randomNumbersHolder = []
+    let randomNumbersHolder = []
     for (let i = 0; i < generateRandomInt(3,5); i++) {
         let randomNumber = generateRandomInt(0, 100)
         while (randomNumbersHolder.includes(randomNumber)) {
