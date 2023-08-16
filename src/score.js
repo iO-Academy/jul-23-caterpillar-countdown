99e154d446bfaee4fcf96070d44368d4fc484277
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-16 11:09:54 +0100

    Put score add into function

--- src/score.js
+++ src/score.js
@@ -1,6 +1,9 @@
+function scoreCount() {
 count = 0
 document.querySelector('.number-cards-container').addEventListener('click', () =>{
     count +=1
     document.querySelector('.score').textContent = 'Score: ' + count
     console.log(count)
 })
+}
+scoreCount()
