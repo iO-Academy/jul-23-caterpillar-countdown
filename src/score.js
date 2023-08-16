1c9bcab17e79d9284d04baa23e815c81b6131d74
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-16 10:56:08 +0100

    Add point to score on card click

---
+++ src/score.js
@@ -0,0 +1,6 @@
+count = 0
+document.querySelector('.number-cards-container').addEventListener('click', () =>{
+    count +=1
+    document.querySelector('.score').textContent = 'Score: ' + count
+    console.log(count)
+})
