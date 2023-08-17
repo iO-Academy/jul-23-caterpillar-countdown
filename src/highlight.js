7158f5b83d3f97fdfb4b323cdc86da98bc5f24b8
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-17 10:57:29 +0100

    actually made the numbers disappear now if all click lol

--- src/highlight.js
+++ src/highlight.js
@@ -2,17 +2,28 @@ const cards = document.querySelectorAll(".number-card")
 let randomNumbersInSequence = randomNumbersHolder.sort((function(a, b){return a-b}))
 let correctNumbersHolder = []
 
+function removeCards () {
+  document.querySelector('.number-cards-container').innerHTML = ""
+}
+
 cards.forEach(function (card) {
   card.addEventListener("click", (e) => {
     if (e.target.textContent == Math.min(...randomNumbersInSequence)) {
-      e.target.style.backgroundColor = "#686868"
-      correctNumbersHolder.push(randomNumbersInSequence.shift(randomNumbersInSequence))
+        if (randomNumbersInSequence.length == 1) {
+          removeCards()
+        } else {
+          e.target.style.backgroundColor = "#686868"
+          correctNumbersHolder.push(randomNumbersInSequence.shift(randomNumbersInSequence))
+          console.log(randomNumbersInSequence)     
+        }
     } else {
-      randomNumbersInSequence = correctNumbersHolder.concat(randomNumbersInSequence)
-      correctNumbersHolder = [] 
-      cards.forEach(card => {
+        randomNumbersInSequence = correctNumbersHolder.concat(randomNumbersInSequence)
+        correctNumbersHolder = [] 
+        cards.forEach(card => {
         card.style.backgroundColor = "#cd853f"
-      })
-    }
+    })}    
   })
 })
+  
+      
+      
