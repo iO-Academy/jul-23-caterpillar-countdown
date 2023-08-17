e8cdf4fc98c43ad3386568b9b55e25622fb3ed0c
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-17 11:30:42 +0100

    Story6Task1 and Task3 function restart the round

---
+++ src/newSetOfNumbers.js
@@ -0,0 +1,24 @@
+function newGameRound() {
+    while(randomNumbersHolderSequential.length == 0 && timer !=0)
+     generate3To5NumberCards()
+}
+
+
+
+
+
+// function newSetofNumbers(){
+//    if (randomNumbersHolderSequential.length != 0) {
+//    generate3To5NumberCards()
+
+// }
+// }
+// newSetofNumbers()
+// function startANewRound() {
+//     const newRound = generate3To5NumberCards()
+//     i = 0
+//     for (let i = 0; i < randomNumbersHolderSequential.length; i++) {
+// (randomNumbersHolderSequential.length == 0) 
+//    generate3To5NumberCards()
+// }
+//     }
